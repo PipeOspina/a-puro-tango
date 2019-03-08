a9757eb0d69292e6060004de33d55d336a4709ac
Felipe Ospina <pipe.98.oh@gmail.com>
2019-03-08 16:52:47 -0500

    Creating first view

--- app/src/main/java/com/example/a_puro_tango/MainActivity.kt
+++ app/src/main/java/com/example/a_puro_tango/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.a_puro_tango
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+//import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
     }
+
+
 }
