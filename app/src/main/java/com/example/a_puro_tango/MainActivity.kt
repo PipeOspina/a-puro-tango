d52f0576456558c28bbe819a591f04741f32a0c1
Felipe Ospina <pipe.98.oh@gmail.com>
2019-03-08 15:38:52 -0500

    First Commit

---
+++ app/src/main/java/com/example/a_puro_tango/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a_puro_tango
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
